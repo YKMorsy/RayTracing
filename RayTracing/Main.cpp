a6af0709fb769fecc1bed2d2d3cbb93fa3524c45
Yasser Morsy <yasser.morsy@live.com>
2022-01-01 03:20:24 -0500

    Add initial sphere creating functions

--- RayTracing/Main.cpp
+++ RayTracing/Main.cpp
@@ -1,18 +1,11 @@
-#include "color.h"
+﻿#include "color.h"
 #include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
-// Create ray_color function that takes in ray object and returns color object
-color ray_color(const ray& r)
-{
-    // Unit direction object of ray r
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    // Blends between blue and white (0 is white, 1 is blue, 0.0 < t < 1.0)
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
-}
+color ray_color(const ray& r);
+bool hit_sphere(const point3& center, double radius, const ray& r);
 
 int main() {
 
@@ -44,7 +37,7 @@ int main() {
             double u = double(i) / (image_width - 1);
             double v = double(j) / (image_height - 1);
 
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            ray r = ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = ray_color(r);
             // Use write_color to scale back to normal RGB scale
             write_color(std::cout, pixel_color);
@@ -70,4 +63,32 @@ int main() {
     // Print error to console (will not be directed to .ppm file)
     // Use .\RayTracing.ppm 2> Image.txt if you want to direct to .txt
     std::cerr << "\nDone.\n";
+}
+
+// Create ray_color function that takes in ray object and returns color object
+color ray_color(const ray& r)
+{
+    // Draw sphere if hit_sphere returns true
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
+    // Unit direction object of ray r
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    // Blends between blue and white (0 is white, 1 is blue, 0.0 < t < 1.0)
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+// Sphere creation method
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    // t^2*b*b + 2*t*b*(A−C) + (A−C)*(A−C)−r^2=0
+    vec3 oc = r.origin() - center; // (A−C)
+    auto a = dot(r.direction(), r.direction()); // b*b
+    auto b = 2.0 * dot(oc, r.direction()); // 2*b*(A−C)
+    auto c = dot(oc, oc) - radius * radius; // (A−C)*(A−C)−r^2
+    // positive or negative of discriminant determines if sphere is hit (how many root there are)
+    auto discriminant = b * b - 4 * a * c; 
+    return (discriminant > 0);
 }
